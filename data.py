51511b820289cfa11ceecab9d340dad3cbef7c5f
fhunter <fhunter@fhunter.link>
2024-03-11 13:05:16 +0100

    Improved filtering

--- data.py
+++ data.py
@@ -3,9 +3,14 @@
 def filter_hardware_report(data_in):
     """ Filter incoming data for frequently changeable attributes
     not relevant to hardware configuration (like current cpu frequency) """
-    if "id" in data_in:
-        if data_in["id"] == "cpu":
-            del data_in["size"]
+    if data_in.get("id") == "cpu":
+        del data_in["size"]
+    if data_in.get("class") == "volume":
+        if "configuration" in data_in:
+            if "modified" in data_in["configuration"]:
+                del data_in["configuration"]["modified"]
+            if "mounted" in data_in["configuration"]:
+                del data_in["configuration"]["mounted"]
     if "children" in data_in:
         for i in data_in["children"]:
             filter_hardware_report(i)
